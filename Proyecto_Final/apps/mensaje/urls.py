11c0eddfaa24452dd47bbaf245da983ea17f3513
Agustin3555 <Agustin3555@hotmail.com>
2021-03-17 16:35:26 -0300

    Se pueden enviar mensajes entre usuarios, pero no se crean aun automaticamente un chat

--- Proyecto_Final/apps/mensaje/urls.py
+++ Proyecto_Final/apps/mensaje/urls.py
@@ -5,6 +5,8 @@ from apps.mensaje import views
 urlpatterns = [
     path('enviar_correo/', views.enviar_correo, name='enviar_correo'),
     path('gracias/', views.gracias, name='gracias'),
+
     path('lista_de_chats/', views.lista_de_chats, name='lista_de_chats'),
     path('chat/<int:id_chat>', views.chat, name='chat'),
+    path('enviar_mensaje/<int:id_chat>', views.enviar_mensaje, name='enviar_mensaje'),
 ]
--- Proyecto_Final/apps/mensaje/views.py
+++ Proyecto_Final/apps/mensaje/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.conf import settings
 from django.core.mail import send_mail
 from apps.mensaje.models import Chat
+from apps.mensaje.models import Mensaje
 
 
 def enviar_correo (request):
@@ -30,15 +31,33 @@ def gracias(request):
 
 def lista_de_chats(request):
 
-    context = {'chats': Chat.objects.filter(usuario_creador=request.user)}
+    c_del_creador = Chat.objects.filter(usuario_creador=request.user)
+    c_del_futuro_duenio = Chat.objects.filter(usuario_futuro_duenio=request.user)
 
-    return render(request, "mensaje/lista_de_chats.html", context)
+    context = {'chats': c_del_creador.union(c_del_futuro_duenio).order_by('-id_chat')}
+
+    return render(request, 'mensaje/lista_de_chats.html', context)
 
 
 def chat(request, id_chat: int):
 
+    context = {'mensajes': Mensaje.objects.filter(chat=id_chat),
+               'id_chat': id_chat}
+
+    return render(request, 'mensaje/chat.html', context)
+
+
+def enviar_mensaje(request, id_chat: int):
+
     context = {}
 
-    print(id_chat)
+    if request.GET['mensaje']:
 
-    return render(request, "mensaje/lista_de_chats.html", context)
+        i_chat = Chat.objects.filter(id_chat=id_chat)[0]
+
+        mensaje = Mensaje(chat=i_chat, usuario_emisor=request.user, mensaje=request.GET['mensaje'])
+        mensaje.save()
+
+        return redirect(to='http://127.0.0.1:8000/chat/' + id_chat.__str__())
+
+    return render(request, 'mensaje/enviar_mensaje.html', context)
