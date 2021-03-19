10afb726e521374c7385ae4528d7b3c8a92040d9
Agustin3555 <79415661+Agustin3555@users.noreply.github.com>
2021-03-19 20:51:00 -0300

    Update views.py

--- Proyecto_Final/apps/postulante/views.py
+++ Proyecto_Final/apps/postulante/views.py
@@ -2,24 +2,30 @@ from django.shortcuts import render, redirect
 from apps.postulante.models import Postulante
 from apps.publicacion.models import Publicacion
 
-def ver_postulantes(request,publicacion_id):
-    context = {}
+def ver_postulantes(request, id_publicacion):
 
-    publicacion = Publicacion.objects.get(id_publicacion=publicacion_id)
-    postulante = Postulante.objects.filter(publicacion=publicacion,usuario_postulado=request.user)
-    postulantes = Postulante.objects.filter(publicacion=publicacion_id)
-    
-    if request.method == 'POST':
-    
-        if not postulante.exists():
-    
-            postulacion = Postulante(publicacion=publicacion, usuario_postulado=request.user)
-            postulacion.save()
-        
-            return redirect ('ver_postulantes',publicacion_id)
-    context = {'postulantes' : postulantes,'publicacion' : publicacion}
+    postulantes = Postulante.objects.filter(publicacion=id_publicacion)
+    publicacion = Publicacion.objects.get(id_publicacion=id_publicacion)
 
-    return render(request,'postulante/ver_postulantes.html',context)
+    context = {'postulantes': postulantes, 'publicacion': publicacion}
+
+    try:
+        if request.GET['postularse']:
+
+            postulante = Postulante.objects.filter(publicacion=publicacion, usuario_postulado=request.user)
+
+            if not postulante.exists():
+
+                postulacion = Postulante(publicacion=publicacion, usuario_postulado=request.user)
+                postulacion.save()
+
+                return redirect('ver_postulantes', id_publicacion)
+
+    except MultiValueDictKeyError:
+        pass
+
+    return render(request, 'postulante/ver_postulantes.html', context)
+
 
 def ver_mis_postulaciones(request):
 
