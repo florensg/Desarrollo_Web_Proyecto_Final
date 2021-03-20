7ec092671b75dd5650434070e19661bbfb6d9804
Agustin3555 <79415661+Agustin3555@users.noreply.github.com>
2021-03-19 21:42:44 -0300

    Update urls.py

--- Proyecto_Final/apps/postulante/urls.py
+++ Proyecto_Final/apps/postulante/urls.py
@@ -3,7 +3,7 @@ from apps.postulante import views
 
 
 urlpatterns = [
-    path('ver_postulantes/<int:publicacion_id>', views.ver_postulantes, name='ver_postulantes'),
+    path('ver_postulantes/<int:id_publicacion>', views.ver_postulantes, name='ver_postulantes'),
     path('ver_mis_postulaciones/',views.ver_mis_postulaciones, name='ver_mis_postulaciones'),
     path('elegir_duenio/<int:id_publicacion>', views.elegir_duenio, name='elegir_duenio'),
     path('hacer_duenio/', views.hacer_duenio, name='hacer_duenio'),
